83676604b71f6fb8096168d562ab87e33555b153
Saurabh Kumar <129621065+SaurabhKr002@users.noreply.github.com>
2025-02-16 14:28:18 +0530

    Saurabh . Add PasswordValidator

---
+++ day5/src/main/java/junit/advancejunittest/security/PasswordValidator.java
@@ -0,0 +1,22 @@
+package junit.advancejunittest.security;
+
+public class PasswordValidator {
+
+    public static boolean isValidPassword(String password) {
+        if (password == null || password.length() < 8) {
+            return false;
+        }
+        boolean hasUpperCase = false;
+        boolean hasDigit = false;
+
+        for (char ch : password.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                hasUpperCase = true;
+            }
+            if (Character.isDigit(ch)) {
+                hasDigit = true;
+            }
+        }
+        return hasUpperCase && hasDigit;
+    }
+}
